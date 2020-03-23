68a8adbb7722f0972f6d362a333274a4c4df4cc5
Tom Clark <tom@octue.com>
2020-03-23 15:52:16 +0000

    SEO Added rudimentary site metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,8 @@ import Footer from './Footer';
 
 export default class Body extends React.Component {
     render() {
+        const title = _.get(this.props, 'pageContext.frontmatter.title') || _.get(this.props, 'pageContext.site.siteMetadata.title')
+        const description = _.get(this.props, 'pageContext.frontmatter.subtitle') || 'A site for engineers to help healthcare professionals with their PPE'
         return (
             <React.Fragment>
                 <Helmet>
@@ -17,6 +19,20 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i%26Display=swap" rel="stylesheet"/> 
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+
+                    <meta property="og:title" content={title} />
+                    <meta name="twitter:title" content={title}/>
+                    <meta name="description" content={description}/>
+                    <meta property="og:description" content={description}/>
+                    <meta name="twitter:description" content={description}/>
+                    <meta name="twitter:site" content="@tom_at_octue"/>
+                    <meta name="twitter:card" content="summary"/>
+                    <meta property="og:locale" content="en_EN"/>
+                    <meta property="og:type" content="article"/>
+                    <meta property="og:site_name" content="Engineers for Doctors"/>
+                    <meta property="og:image" content="https://www.datocms-assets.com/24377/1584618093-img6596.jpg?w=1000&fit=max&fm=jpg" />
+                    <meta name="twitter:image" content="https://www.datocms-assets.com/24377/1584618093-img6596.jpg?w=1000&fit=max&fm=jpg" />
+
                 </Helmet>
                 <div id="page" className="site">
                   <Header {...this.props} />
@@ -29,3 +45,5 @@ export default class Body extends React.Component {
         );
     }
 }
+
+
